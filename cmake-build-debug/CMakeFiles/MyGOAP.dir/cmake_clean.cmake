2d59008aaca1803fafc9f2f889e3c8a60329018a
Foiros <arttu.p_90@hotmail.com>
2021-10-07 15:27:37 +0300

    1. Actual Initial Commit 7.10.2021

---
+++ cmake-build-debug/CMakeFiles/MyGOAP.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyGOAP.dir/main.cpp.obj"
+  "MyGOAP.exe"
+  "MyGOAP.exe.manifest"
+  "MyGOAP.lib"
+  "MyGOAP.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyGOAP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
